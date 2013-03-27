d90888f96bdc08d8a4a781263bcc543642c8430e
Eileen <eileen.li93@gmail.com>
2013-03-27 16:11:35 -0700

    first commit

---
+++ ribbit/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'ribbit_app.views.index'), # root
+    url(r'^login$', 'ribbit_app.views.login_view'), # login
+    url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
+    url(r'^ribbits$', 'ribbit_app.views.public'), # public ribbits
+    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit
+    url(r'^users/$', 'ribbit_app.views.users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
+    url(r'^follow$', 'ribbit_app.views.follow'),
+)  
