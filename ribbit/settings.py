ef0638a81906f9d642ef4fdc6344e5192f60f4df
Eileen <eileen.li93@gmail.com>
2013-03-29 17:29:29 -0700

    blah

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -127,7 +127,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
     'ribbit_app',
-    'gunicorn',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
