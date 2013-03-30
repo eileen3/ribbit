b625515d0e4ca834d6c8992e8b8a04b8bbf1b7a3
Eileen <eileen.li93@gmail.com>
2013-03-30 00:34:20 -0700

    production

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -15,10 +15,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, 'database.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ribbit',                      # Or path to database file if using sqlite3.
+        'USER': 'username',                      # Not used with sqlite3.
+        'PASSWORD': 'password',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -161,6 +161,6 @@ LOGGING = {
     }
 }
 
-# import dj_database_url
+import dj_database_url
  
-#  DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
